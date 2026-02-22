5b9c878cee0eb708cbf7388cdb20cc33d70b793b
Sheldon <calicottsheldon@gmail.com>
2026-02-22 15:39:50 -0500

    Remove stray 'fe' symbol from ReportsPage.jsx

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import pkg from 'pg';
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const pkg = require('pg');
 const { calculateTargetProductivity } = require('./targetUtils');
 
 const { Pool } = pkg;
